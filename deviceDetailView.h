5f8bfce8a8493e755c1e34b5d9dc59e79d5fe7aa
Daniel Avram <daniel.e.avram@gmail.com>
2013-12-01 17:06:04 -0500

    added unlock method to button

---
+++ deviceDetailView.h
@@ -0,0 +1,13 @@
+//
+//  deviceDetailView.h
+//  HAUS
+//
+//  Created by Daniel Avram on 12/1/13.
+//  Copyright (c) 2013 Daniel Avram. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface deviceDetailView : NSObject
+
+@end
